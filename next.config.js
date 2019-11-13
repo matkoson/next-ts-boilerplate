2577e324c590cb2d2e945a27be6cb6eebddec575
matkoson <mateusz.koson@hotmail.com>
2019-11-13 10:11:22 +0100

    feat: add next env vals

---
+++ next.config.js
@@ -0,0 +1,15 @@
+exports.default = {
+	env: {
+		VERSION: 'dev',
+		APP_NAME: 'Connectico',
+		CLIENT_URL: 'http://localhost:3000',
+		API_URL: 'http://localhost:3001/api/v2',
+		INSPECTLET_ID: '',
+		AMPLITUDE_KEY: '',
+		INTERCOM_ID: '',
+		ANALYTICS_ID: '',
+		SENTRY_DSN: '',
+		DEV: true,
+		PAYPAL_ENV: 'sandbox'
+	}
+};
